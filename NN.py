807fb16123d4e44af23e65112e22374ef52901c7
Alexandre <149970257+Alexandre1609-bit@users.noreply.github.com>
2025-11-11 15:55:53 +0100

    Third commit
    Ajout des fonctions d'activation "ReLU" et "Softmax".  Ajout de la "Forward Propagation". Ajout de divers commentaires contenant nombre d'explications, d'analogies sur le fonctionnement du code et d'un réseau neuronal dans sa globalité.

--- NN.py
+++ NN.py
@@ -71,6 +71,66 @@ def init_params():
     DONC la seule forme possible de W2 est de (3, 10)'''
 
 
-    #Activation (Rendre le tout "non-linéaire"(ReLu? SoftMax?)) :
-    
+    #Activation (Rendre le tout "non-linéaire"(ReLu? SoftMax?, Sigmoïde?, initialisation de Z1, Z2... ?? )) :
+    '''Fonction d'activation. ReLu regarde si un résultat est < 0 ou > 0. 
+    Si Z est négatif (-1.5) alors ReLu dira "Ce n'est pas assez fort, j'ignore" et renverra un 0, le neurone sera "éteint.
+    Dans le cas contraire, si Z est positif (3.8) alors ReLu dit "Ce signal est important, je le laisse passer tel quel. 
+    Il renvoie 3.8, le neurone est "allumé".'''
 
+def relu(Z):  #Erreur, pas mettre relu(Z1) mais plutôt un nom générique comme relu(Z), pareil pour softmax.
+    ReLu = np.maximum(0.0, Z)
+    return ReLu
+
+def softmax(Z):
+     numerateur = np.exp(Z)
+     denominateur = np.sum(np.exp(Z), axis=0, keepdims=True)
+     sm = numerateur / denominateur
+     return sm
+
+
+     '''Pour softmax : C'est une des fonctions d'activation standard pour la couche finale d'un problème de classification multi-classes (Ici 3 iris).
+    On peut voir cette fonction comme un "traducteur" qui convertit les scores bruts en bulletin de confiance.
+    La couche finale calcule ses scores bruts Z2, pour une fleurs ces scores (logits) pourraient être par exemple :
+    [Score Setosa : 1.2], [Score Versicolor ; -0.8], [Score Virginica : 4.1].
+    Le problème est que nous ne savons pas interpreter ces scores.
+    4.1 est élevé mais est-ce 50% de confiance, 90%, 100% ?? Et comment gérer les scores négatifs ??
+    
+    -1"Exponentielle"
+    
+    C'est ici que softmax intervient. La fonction rend tous les scores positifs en utilisant (np.exp()).
+    Donc nos scores deviennent [3.32, 0.45, 60.34]. On voit que l'écart entre le score le plus haut et les autres est énorme.
+
+    -2"Normalisation"
+
+    Ensuite la fonction divise chaque score par la somme de tous les scores (3.32 + 0.45 + 60.34 = 64.11)
+    On obtient : 
+    Setosa : 3.32 / 64.11 = 0.05 (soit 5%)
+    Versicolor : 0.45 / 64.11 = 0.007 (soit 0.7%)
+    Virginica : 60.34 / 64.11 = 0.94 (soit 94%)
+
+    Les resultats nous montrent une prédiction claire : [0.05, 0.007, 0.94].
+    C'est un vecteur de probabilité dont la somme fait 1 !
+
+    On devient donc en mesure d'interpréter les résultats (pourcentages de confiance, pas de score bruts.)
+    C'est aussi "soft", un Hard Max aurait donné [0, 0, 1]. Avec Softmax c'est plus nuancé.
+    On voit qu'il y a 94% de chance que ce soit Virginica mais qu'il y ait aussi 5% de chance que ce soit Setosa.
+    Cette nuance est vitale pour l'apprentissage. (Rétropropagation)
+
+    '''
+
+     #Forward Propagation (Forward Pass?)
+     '''Se renseigner sur le sujet.'''
+
+def forward_prop(X, W1, b1, W2, b2):
+    
+    Z1 = W1 @ X + b1 #Erreur : pas * mais @ car c'est l'opérateur de NumpY pour la multiplication matricielle
+    A1 = relu(Z1)
+    
+    Z2 = W2 @ A1 + b2 #Erreur : Pas  Z2 = W2 @ X_train + b2 car Z2 prend les résultat de Z1 (donc A1), c'est une chaîne !
+    A2 = softmax(Z2)
+
+    return A1, A2, Z1, Z2
+
+#Prochaine étape : Fonction de coût : (Loss function)
+'''Se renseigner dessus. Analogie du "Juge".
+Cette fonction va regarder la prédiction A2 et la comparer au vraies réponses (Y_train) afin de donner une "note d'erreur" (le "coût" ou "loss")'''
